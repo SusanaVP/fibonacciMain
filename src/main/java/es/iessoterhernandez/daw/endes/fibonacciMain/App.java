a346cf1d020317bf4b130969818468a7167776c4
SusanaVP <120553037+SusanaVP@users.noreply.github.com>
2022-12-14 09:03:27 +0100

    fibonacciMain

---
+++ src/main/java/es/iessoterhernandez/daw/endes/fibonacciMain/App.java
@@ -0,0 +1,16 @@
+package es.iessoterhernandez.daw.endes.fibonacciMain;
+
+import es.iessoterhernandez.daw.endes.fibonacci.*;
+public class App 
+{
+	
+    public static void main( String[] args )
+    {    
+    	ClaseFibonacci a = new ClaseFibonacci();
+    		  int count=30;    
+    		  System.out.print(0+" "+1);//printing 0 and 1    
+    		  a.printFibonacci(count-2);
+    		
+       
+    }
+}
